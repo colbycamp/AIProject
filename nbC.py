d89fb599d2820dcbcbb2b0e4ec54898d284f2b57
Jae Son <sjson421@gmail.com>
2018-04-23 23:53:01 -0400

    Refactoring

--- nbC.py
+++ nbC.py
@@ -1,104 +1,91 @@
-# Example of Naive Bayes implemented from Scratch in Python
 import csv
 import random
 import math
- 
-def loadCsv(filename):
-	lines = csv.reader(open(filename, "rb"))
-	dataset = list(lines)
-	for i in range(len(dataset)):
-		dataset[i] = [float(x) for x in dataset[i]]
-        #dataset[i] = [abs(x)for x in dataset[i]]
-	return dataset
- 
-def splitDataset(dataset, splitRatio):
-	trainSize = int(len(dataset) * splitRatio)
-	trainSet = []
-	copy = list(dataset)
-	while len(trainSet) < trainSize:
-		index = random.randrange(len(copy))
-		trainSet.append(copy.pop(index))
-	return [trainSet, copy]
- 
-def separateByClass(dataset):
-	separated = {}
-	for i in range(len(dataset)):
-		vector = dataset[i]
-		if (vector[-1] not in separated):
-			separated[vector[-1]] = []
-		separated[vector[-1]].append(vector)
-	return separated
- 
+
 def mean(numbers):
 	return sum(numbers)/float(len(numbers))
- 
+
 def stdev(numbers):
-    y = len(numbers)
-    print(y)
-    avg = mean(numbers)
-    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    variance = sum([(x-mean(numbers))**2 for x in numbers])/float(len(numbers)-1)
     return math.sqrt(variance)
- 
-def summarize(dataset):
-	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-	del summaries[-1]
-	return summaries
- 
-def summarizeByClass(dataset):
-	separated = separateByClass(dataset)
-	summaries = {}
-	for classValue, instances in separated.iteritems():
-		summaries[classValue] = summarize(instances)
-	return summaries
- 
-def calculateProbability(x, mean, stdev):
-	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
- 
-def calculateClassProbabilities(summaries, inputVector):
+
+def split(dataset, splitRatio):
+    trainingSet = []
+    trainingSetSize = int(len(dataset) * splitRatio)
+
+    temp = list(dataset)
+    while len(trainingSet) < trainingSetSize:
+        trainingSet.append(temp.pop(random.randrange(len(temp))))
+    return [trainingSet, temp]
+
+def getMeanStDev(dataset):
+	results = [(mean(i), stdev(i)) for i in zip(*dataset)]
+	del results[-1]
+	return results
+
+def getMeanStDevByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+
+    results = {}
+    for genre, instances in separated.iteritems():
+        temp = [(mean(i), stdev(i)) for i in zip(*instances)]
+        del temp[-1]
+        results[genre] = temp
+    return results
+
+def getProbabilitiesByClass(results, inputVector):
 	probabilities = {}
-	for classValue, classSummaries in summaries.iteritems():
-		probabilities[classValue] = 1
+	for genre, classSummaries in results.iteritems():
+		probabilities[genre] = 1
 		for i in range(len(classSummaries)):
 			mean, stdev = classSummaries[i]
 			x = inputVector[i]
-			probabilities[classValue] *= calculateProbability(x, mean, stdev)
+			probabilities[genre] *= math.exp(-((x-mean)**2/(2*stdev**2))) * (1/(math.sqrt(2*math.pi) * stdev))
 	return probabilities
 			
-def predict(summaries, inputVector):
-	probabilities = calculateClassProbabilities(summaries, inputVector)
-	bestLabel, bestProb = None, -1
-	for classValue, probability in probabilities.iteritems():
-		if bestLabel is None or probability > bestProb:
-			bestProb = probability
-			bestLabel = classValue
-	return bestLabel
- 
-def getPredictions(summaries, testSet):
+def predict(results, inputVector):
+    probabilities = getProbabilitiesByClass(results, inputVector)
+    bestGenre = None
+    topProbability = -1.0
+    for genre, probability in probabilities.iteritems():
+        if probability > topProbability:
+            bestGenre = genre
+            topProbability = probability
+    return bestGenre
+
+def getPredictions(results, testSet):
 	predictions = []
 	for i in range(len(testSet)):
-		result = predict(summaries, testSet[i])
-		predictions.append(result)
+		p = predict(results, testSet[i])
+		predictions.append(p)
 	return predictions
- 
+
 def getAccuracy(testSet, predictions):
 	correct = 0
 	for i in range(len(testSet)):
 		if testSet[i][-1] == predictions[i]:
 			correct += 1
 	return (correct/float(len(testSet))) * 100.0
- 
+
 def main():
-	filename = 'fma_metadata/mfcc_dataset.csv'
-	splitRatio = 0.75
-	dataset = loadCsv(filename)
-	trainingSet, testSet = splitDataset(dataset, splitRatio)
-	print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
-	# prepare model
-	summaries = summarizeByClass(trainingSet)
-	# test model
-	predictions = getPredictions(summaries, testSet)
-	accuracy = getAccuracy(testSet, predictions)
-	print('Accuracy: {0}%').format(accuracy)
- 
+    file = 'fma_metadata/mfcc_dataset.csv'
+    
+    lines = csv.reader(open(file, "rb"))
+    dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+
+    trainingSet, testSet = split(dataset, 0.75)
+    print('Data set of size',len(dataset),'split into:')
+    print('Training Set:',len(trainingSet),'rows')
+    print('Test Set:',len(testSet),'rows')
+
+    accuracy = getAccuracy(testSet, getPredictions(getMeanStDev(trainingSet), testSet))
+    print('Accuracy: {0}%').format(accuracy)
+
 main()
