20a9e13109a609e9ff5e91a0b577d3c8f590dae7
Jae Son <sjson421@gmail.com>
2018-04-24 19:30:10 -0400

    Really fixed

--- nbC.py
+++ nbC.py
@@ -6,7 +6,9 @@ def mean(numbers):
 	return sum(numbers)/float(len(numbers))
 
 def stdev(numbers):
-    variance = sum([(x-mean(numbers))**2 for x in numbers])/float(len(numbers)-1)
+    print("Training...")
+    avg = mean(numbers)
+    variance = sum([(x-avg)**2 for x in numbers])/float(len(numbers)-1)
     return math.sqrt(variance)
 
 def split(dataset, splitRatio):
@@ -27,7 +29,7 @@ def getMeanStDevByClass(dataset):
         separated[vector[-1]].append(vector)
 
     results = {}
-    for genre, instances in separated.iteritems():
+    for genre, instances in separated.items():
         temp = [(mean(i), stdev(i)) for i in zip(*instances)]
         del temp[-1]
         results[genre] = temp
@@ -35,30 +37,31 @@ def getMeanStDevByClass(dataset):
 
 def getProbabilitiesByClass(results, inputVector):
 	probabilities = {}
-	for genre, classSummaries in results.iteritems():
+	for genre, classSummaries in results.items():
 		probabilities[genre] = 1
 		for i in range(len(classSummaries)):
 			mean, stdev = classSummaries[i]
 			x = inputVector[i]
 			probabilities[genre] *= math.exp(-((x-mean)**2/(2*stdev**2))) * (1/(math.sqrt(2*math.pi) * stdev))
 	return probabilities
-			
+		
 def predict(results, inputVector):
     probabilities = getProbabilitiesByClass(results, inputVector)
     bestGenre = None
     topProbability = -1.0
-    for genre, probability in probabilities.iteritems():
+    for genre, probability in probabilities.items():
         if probability > topProbability:
             bestGenre = genre
             topProbability = probability
+    print("Testing...")
     return bestGenre
 
 def getPredictions(results, testSet):
-	predictions = []
-	for i in range(len(testSet)):
-		p = predict(results, testSet[i])
-		predictions.append(p)
-	return predictions
+    predictions = []
+    for i in range(len(testSet)):
+        p = predict(results, testSet[i])
+        predictions.append(p)
+    return predictions
 
 def getAccuracy(testSet, predictions):
 	correct = 0
@@ -75,7 +78,7 @@ def main():
     for i in range(len(dataset)):
         dataset[i] = [float(x) for x in dataset[i]]
 
-    trainingSet, testSet = split(dataset, 0.75)
+    trainingSet, testSet = split(dataset, 0.9)
     print('Data set of size {0} split into:'.format(len(dataset)))
     print('Training Set: {0} rows'.format(len(trainingSet)))
     print('Test Set: {0} rows'.format(len(testSet)))
